596edb3b5a66ad3f353811d8ea69d1712783854e
AbrarNoob <hasinabrarhaque@gmail.com>
2023-04-29 15:26:53 +0600

    alioth: DerpFest

--- derp_alioth.mk
+++ derp_alioth.mk
@@ -8,8 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SuperiorOS stuff.
-$(call inherit-product, vendor/superior/config/common.mk)
+# Inherit some common Derp stuff.
+$(call inherit-product, vendor/derp/config/common_full_phone.mk)
+
+#Derp Stuffs
+TARGET_FACE_UNLOCK_SUPPORTED := true
+USE_LEGACY_BOOTANIMATION := true
+DERP_BUILDTYPE := CARROT
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Inherit from alioth device
 $(call inherit-product, device/xiaomi/alioth/device.mk)
@@ -19,17 +25,5 @@ PRODUCT_DEVICE := alioth
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := POCO F3
-
-# Boot resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
-# Supported Device Flags
-TARGET_SUPPORTS_NOW_PLAYING := true
-TARGET_INCLUDE_STOCK_ARCORE := true
-
-#SuperioeOS Stuff
-SUPERIOR_OFFICIAL := true
-BUILD_WITH_GAPPS := true
-TARGET_GAPPS_ARCH := arm64
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
